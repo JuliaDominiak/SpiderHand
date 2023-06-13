12591882ebcd12dca94f72c17125439f113c0168
A_Salata <salata200@gmail.com>
2023-06-13 17:17:18 +0200

    calibrated

--- Scripts and Drivers/Hand/src/main.cpp
+++ Scripts and Drivers/Hand/src/main.cpp
@@ -12,8 +12,8 @@ void setup() {
   hand->addServo("thu", 0, 0, 397, 479);
   hand->addServo("ind", 0, 1, 384, 150);
   hand->addServo("mid", 0, 2, 150, 465);
-  hand->addServo("wri", 0, 3, 150, 510);
-  hand->addServo("wri", 1, 4, 307, 150);
+  hand->addServo("wri", 0, 3, 300, 150);
+  hand->addServo("wri", 1, 4, 607, 300);
   //hand->addServo("wri", 2, 5, 196, 540);
   hand->addServo("elb", 2, 6, 433, 150);
   Conn::begin(1420, hand);
