ebbce10f4d5d8c4ea71f274a4a0fedce210ca52f
A_Salata <salata200@gmail.com>
2023-06-13 17:17:02 +0200

    calibrated

--- Scripts and Drivers/Gauntlet/src/main.cpp
+++ Scripts and Drivers/Gauntlet/src/main.cpp
@@ -9,7 +9,7 @@ void setup()
     Serial.begin(115200);
     Wire.begin();
     gauntlet = Gauntlet(0x24, 0x19, 0x40, 0xff, 0xff, 0x2d, 0xff);
-    gauntlet.setCalibration(-60,-10,-60,-150, -20, -120, 0, 100, 0, 100, 0, 50, -15, 50);
+    gauntlet.setCalibration(-175,-40,-170,-360, -100, -230, 0, 100, 0, 100, -32, 40, -66, 12);
     gauntlet.begin();
     Conn::begin("255.255.255.255", 1420); //port >1024?
 }
