40ab118f92675cee2f0d29d440d1a4b3815475c7
A_Salata <salata200@gmail.com>
2023-06-26 23:19:25 +0200

    reenable other joints

--- Scripts and Drivers/Gauntlet/src/main.cpp
+++ Scripts and Drivers/Gauntlet/src/main.cpp
@@ -10,10 +10,10 @@ void setup()
     Serial.begin(115200);
     Wire.begin();
     gauntlet = Gauntlet(); 
-    //gauntlet.addFlexSensor(FlexSensor("thu", 0x24, -175, -40, 0, 100));
-    //gauntlet.addFlexSensor(FlexSensor("ind", 0x19, -170, -360, 0, 100));
-    //gauntlet.addFlexSensor(FlexSensor("mid", 0x40, -100, -230, 0, 100));
-    //gauntlet.addFlexSensor(FlexSensor("wri", 0x2d, -32, 40, -66, 12));
+    gauntlet.addFlexSensor(FlexSensor("thu", 0x24, -175, -40, 0, 100));
+    gauntlet.addFlexSensor(FlexSensor("ind", 0x19, -170, -360, 0, 100));
+    gauntlet.addFlexSensor(FlexSensor("mid", 0x40, -100, -230, 0, 100));
+    gauntlet.addFlexSensor(FlexSensor("wri", 0x2d, -32, 40, -66, 12));
     gauntlet.addFlexSensor(FlexSensor("elb", 0x33, -220, -90, 0, 100));
     Conn::begin("255.255.255.255", 1420); //port >1024?
 }
